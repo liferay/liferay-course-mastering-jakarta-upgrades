b7cfaad2b8b97a86a125ee382437ed9ef388b792
David Nebinger <david.nebinger@liferay.com>
2026-01-08 15:19:38 -0500

    Created the initial service builder modules

---
+++ modules/clarity-insurance-benefits-tracker/clarity-insurance-benefits-tracker-api/src/main/java/com/clarityvisionsolutions/insurance/benefits/tracker/constants/InsuranceBenefitsTrackerPortletKeys.java
@@ -0,0 +1,9 @@
+package com.clarityvisionsolutions.insurance.benefits.tracker.constants;
+
+public class InsuranceBenefitsTrackerPortletKeys {
+
+    public static final String IBT_DASHBOARD = "clarity_insurance_benefits_tracker_dashboard";
+    public static final String IBT_PLANS_ADMIN = "clarity_insurance_benefits_tracker_plans_admin";
+    public static final String IBT_ENROLLMENT_ADMIN = "clarity_insurance_benefits_tracker_enrollment_admin";
+
+}
